b8329ab0e0b3bb757c77fb03618a4770b2057d30
Chakorn <chakorn0243@gmail.com>
2021-03-22 17:12:18 +0700

    add database

--- hw3/1.php
+++ hw3/1.php
@@ -1,6 +1,6 @@
 <?php
 $dbhost = 'localhost';
-$dbuser ='root';
+$dbuser = 'root';
 $dbpass = '';
 $dbname = 'codephp';
 
@@ -8,16 +8,57 @@ $myconn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 if ($myconn->connect_errno) {
     printf("Connect failed: %s\n", $myconn->connect_error);
     exit();
-} 
-
-$strSQL = "SELECT `userID`, `userName`,`userStatus` FROM `user`";
-$result = $myconn -> query($strSQL);
-while ($row = $result ->fetch_array()){
-    echo $row["userName"] . "<br>";
-    
 }
 
+$strSQL = "SELECT `userID`, `userName`,`userStatus` FROM `user`";
+$result = $myconn->query($strSQL);
 
 
+
+
+
+
+//  while ($row = $result->fetch_array()) {
+//     echo $row["userName"] . "<br>";
+// }
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+   
+
+    <table border="5" width="80%">
+        <tr>
+            <td>ลำดับ</td>
+            <td>ชื่อผู้ใช้</td>
+            <td>สถานะ</td>
+        </tr>
+    <?php
+    while ($row = $result->fetch_array()) {
+         echo $row["userName"] . "<br>";
+         ?>
+         <tr>
+        <td><?php echo $row ["userID"] ?></td>
+        <td><?php echo $row ["userName"]?> </td>
+        <td><?php echo $row ["userStatus"] ?></td>
+         </tr>
+        <?php
+    
+    }
+?>
+    </table>
+
+
+
+
+</body>
+
+</html>
